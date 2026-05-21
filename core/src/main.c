5efbe1264d7eae80f50e47657d9f934342cca3a0
leBronzo1 <swisshiss56@gmail.com>
2026-05-21 16:50:21 -0700

    Project layot and makefile done

---
+++ core/src/main.c
@@ -0,0 +1,10 @@
+#include "../include/types.h"
+#include "../include/renderer.h"
+#include "../include/input.h"
+#include "../include/audio.h"
+#include "../include/ipc.h"
+#include "../include/state.h"
+
+int main() {
+    return 0;
+}
