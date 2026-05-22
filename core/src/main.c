507c55a0efac107fe916d71c955e916737392c8d
leBronzo1 <swisshiss56@gmail.com>
2026-05-21 18:08:21 -0700

    makefile fix  and SDL headers inclusion

--- core/src/main.c
+++ core/src/main.c
@@ -1,3 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef __APPLE__
+    #include <SDL.h>
+    #include <SDL_ttf.h>
+    #include <SDL_image.h>
+    #include <SDL_mixer.h>
+#else
+    #include <SDL2/SDL.h>
+    #include <SDL2/SDL_ttf.h>
+    #include <SDL2/SDL_image.h>
+    #include <SDL2/SDL_mixer.h>
+#endif
+
 #include "../include/types.h"
 #include "../include/renderer.h"
 #include "../include/input.h"
@@ -6,5 +22,9 @@
 #include "../include/state.h"
 
 int main() {
+   if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_JOYSTICK) != 0) {
+        fprintf(stderr, "[myos] SDL_Init failed: %s\n", SDL_GetError());
+        return 1;
+    }
     return 0;
 }
